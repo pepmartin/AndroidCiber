c4d9959bf8bf13aeffa93c43ffc2e411b4ff93d5
jmarboni <jmarboni@gmail.com>
2019-02-09 20:33:59 -0500

    Correct Constraint

--- Layouts/app/src/main/java/com/jmarboni/layouts/MainActivity.java
+++ Layouts/app/src/main/java/com/jmarboni/layouts/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_relative_main);
+        setContentView(R.layout.activity_constraint_main);
     }
 }
